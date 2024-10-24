a57cd3fbb2b9b364346d3fb9f2624cb039fe1348
nachogonzaliaunc <ignacio.gonzalia@mi.unc.edu.ar>
2024-10-24 19:40:30 -0300

    fix design

--- frontend/src/components/countryCard.tsx
+++ frontend/src/components/countryCard.tsx
@@ -18,7 +18,7 @@ const CountryCard = ({ countryData }: CountryCardProps) => {
 
   return (
     <div className='w-[16.5rem] flex flex-col rounded-md shadow-xl' onClick={manageClick}>
-      <img src={countryData.flags.svg} alt={`${countryData.name} flag`} className='rounded-t-md' />
+      <img src={countryData.flags.svg} alt={`${countryData.name} flag`} className='rounded-t-md w-[16.5rem] h-40 object-cover' />
       <div className='mx-6'>
         <h2 className={`mt-6 nunito-sans-800 text-lg leading-[1.625rem] ${darkTextColor}`}>{countryData.name}</h2>
         <div className='mt-4 mb-11 flex flex-col gap-2 text-sm leading-[.875rem] nunito-sans-600'>
--- frontend/src/components/gridCountries.tsx
+++ frontend/src/components/gridCountries.tsx
@@ -4,13 +4,13 @@ import { CountryData } from '../utils/types';
 
 const GridCountries = () => {
   return (
-    <div className='max-w-7xl m-auto'>
-      <div className='w-100 flex flex-row flex-wrap gap-x-16 gap-y-10 items-start justify-start'>
+    <div className='max-w-7xl mx-auto'>
+      <div className='flex flex-row flex-wrap gap-x-16 gap-y-10 items-center justify-center'>
         {
           (data as CountryData[]).map((country) => (
             <CountryCard countryData={country} key={country.alpha3Code} />
           ))
-        };
+        }
       </div>
     </div>
   );
