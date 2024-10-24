5df051b88ecd8a78458b9cb257350bc49444a9cc
nachogonzaliaunc <ignacio.gonzalia@mi.unc.edu.ar>
2024-10-24 18:46:00 -0300

    grid for countries - add responsiveness

--- frontend/src/components/countryCard.tsx
+++ frontend/src/components/countryCard.tsx
@@ -19,7 +19,7 @@ const CountryCard = ({ countryData }: CountryCardProps) => {
   return (
     <div className='w-[16.5rem] flex flex-col rounded-md shadow-xl' onClick={manageClick}>
       <img src={countryData.flags.svg} alt={`${countryData.name} flag`} className='rounded-t-md' />
-      <div className='ml-6'>
+      <div className='mx-6'>
         <h2 className={`mt-6 nunito-sans-800 text-lg leading-[1.625rem] ${darkTextColor}`}>{countryData.name}</h2>
         <div className='mt-4 mb-11 flex flex-col gap-2 text-sm leading-[.875rem] nunito-sans-600'>
           <p>Population: <span className='nunito-sans-300'>{formatNumberWithCommas(countryData.population)}</span></p>
--- frontend/src/components/gridCountries.tsx
+++ frontend/src/components/gridCountries.tsx
@@ -4,12 +4,14 @@ import { CountryData } from '../utils/types';
 
 const GridCountries = () => {
   return (
-    <div className='w-100 flex flex-col items-center gap-10'>
-      {
-        (data as CountryData[]).map((country) => (
-          <CountryCard countryData={country} key={country.alpha3Code} />
-        ))
-      };
+    <div className='max-w-7xl m-auto'>
+      <div className='w-100 flex flex-row flex-wrap gap-x-16 gap-y-10 items-start justify-start'>
+        {
+          (data as CountryData[]).map((country) => (
+            <CountryCard countryData={country} key={country.alpha3Code} />
+          ))
+        };
+      </div>
     </div>
   );
 }
