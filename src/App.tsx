f2f4284d56cbc46514e8d16136a427b13ad37065
Ignacio Gonzalia <igonzalia@liricus.com.ar>
2025-04-25 20:23:15 -0300

    dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import { useEffect } from "react";
+import { useThemeStore } from "./store/themeStore";
+import { Flex, Switch, Typography } from "antd";
 import "./App.css";
 
 function App() {
-  return <p className="text-xl">Hello world!</p>;
+  const { darkMode, toggleDarkMode } = useThemeStore();
+  const { Title } = Typography;
+
+  useEffect(() => {
+    if (darkMode) document.documentElement.setAttribute("data-theme", "dark");
+    else document.documentElement.setAttribute("data-theme", "light");
+  }, [darkMode]);
+
+  return (
+    <Flex
+      vertical
+      justify="center"
+      align="center"
+      gap={"20px"}
+      className="min-h-screen bg-[color:var(--bg)] transition-colors duration-300"
+    >
+      <Title className="!text-[color:var(--text)]">Hola mundo!</Title>
+      <Switch
+        checked={darkMode}
+        onChange={toggleDarkMode}
+        checkedChildren="🌙"
+        unCheckedChildren="☀️"
+      />
+    </Flex>
+  );
 }
 
 export default App;
